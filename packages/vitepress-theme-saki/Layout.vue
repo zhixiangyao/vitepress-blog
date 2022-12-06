acac12279f794702bcae19d10c5cbcb09881ce81
zhixiangyao <blueskyzx@icloud.com>
2022-12-06 16:13:22 +0800

    remove: APlayer

--- packages/vitepress-theme-saki/Layout.vue
+++ packages/vitepress-theme-saki/Layout.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 import Live2d from './components/Live2d.vue'
-import APlayer from './components/APlayer.vue'
 
 const { Layout } = DefaultTheme
 </script>
 
 <template>
   <Live2d />
-  <APlayer />
+
   <Layout />
 </template>
--- packages/vitepress-theme-saki/global.d.ts
+++ packages/vitepress-theme-saki/global.d.ts
@@ -1,25 +1,4 @@
 declare global {
-  class APlayer {
-    constructor(params: {
-      container: HTMLElement
-      fixed: boolean
-      volume: number
-      theme: string
-      lrcType: number
-      audio: {
-        name: string
-        artist: string
-        url: string
-        lrc: string
-        cover: string
-      }[]
-    })
-
-    lrc: {
-      hide(): void
-    }
-  }
-
   class RaindropFX {
     constructor(params: { canvas: HTMLCanvasElement; background: string })
 
@@ -31,7 +10,6 @@ declare global {
   interface Window {
     copy(e: string): void
     initWidget(params: { waifuPath: string; apiPath: string; cdnPath?: string }): unknown
-    APlayer: typeof APlayer
     RaindropFX: typeof RaindropFX
   }
 }
