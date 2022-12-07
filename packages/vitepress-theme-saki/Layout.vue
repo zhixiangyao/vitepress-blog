bcbee18b95496799021d1a9f2f7446dcfdcbd9b6
zhixiangyao <blueskyzx@icloud.com>
2022-12-07 11:56:41 +0800

    feat: NProgress

--- packages/vitepress-theme-saki/Layout.vue
+++ packages/vitepress-theme-saki/Layout.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
+
 import Live2d from './components/Live2d.vue'
+import NProgress from './components/NProgress.vue'
 
 const { Layout } = DefaultTheme
 </script>
 
 <template>
   <Live2d />
+  <NProgress />
 
   <Layout />
 </template>
---
+++ packages/vitepress-theme-saki/components/NProgress.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+import { useData } from 'vitepress'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+const { page } = useData()
+
+watch(
+  () => page.value.relativePath,
+  () => {
+    NProgress.start()
+
+    setTimeout(() => {
+      NProgress.done()
+    }, 50)
+  },
+)
+</script>
