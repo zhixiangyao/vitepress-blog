f89903c73e68e6b7542efd6380144463cbc96393
zhixiangyao <blueskyzx@icloud.com>
2023-03-24 17:30:31 +0800

    remove: `Live2d`

--- packages/vitepress-theme-saki/Layout.vue
+++ packages/vitepress-theme-saki/Layout.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 
-import Live2d from './components/Live2d.vue'
 import GoogleTag from './components/GoogleTag.vue'
 
 const { Layout } = DefaultTheme
 </script>
 
 <template>
-  <Live2d />
   <GoogleTag />
 
   <Layout />
