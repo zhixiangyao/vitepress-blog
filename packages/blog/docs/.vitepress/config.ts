eb8383917e6215994440e395a48105e25644b85a
zhixiangyao <blueskyzx@icloud.com>
2023-08-28 11:11:02 +0800

    feat: Add favicons

--- packages/blog/docs/.vitepress/config.ts
+++ packages/blog/docs/.vitepress/config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
 
   themeConfig: {
     siteTitle: 'Zhixiang Yao',
+    logo: '/avatar.jpeg',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/zhixiangyao' },
       { icon: 'twitter', link: 'https://twitter.com/yaozhixiang' },
@@ -25,6 +26,24 @@ export default defineConfig({
     //   indexName: 'vitepress_blog',
     // },
   },
+
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+    [
+      'link',
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+    ],
+    [
+      'link',
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+    ],
+    ['link', { rel: 'manifest', href: '/favicons/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#3a0839' }],
+    ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3a0839' }],
+    ['meta', { name: 'msapplication-config', content: '/favicons/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
 })
 
 type Nav = DefaultTheme.Config['nav']
