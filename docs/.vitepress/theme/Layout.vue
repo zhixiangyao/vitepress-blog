5bac4f54afebcdf3e533c9f8562794089bede9ac
zhixiangyao <blueskyzx@icloud.com>
2026-04-24 10:39:34 +0800

    refactor: remove GoogleTag component and its usage from Layout

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 
-import GoogleTag from './components/GoogleTag.vue'
-
 const { Layout } = DefaultTheme
 </script>
 
 <template>
-  <GoogleTag />
-
   <Layout class="layout" />
 </template>
 
